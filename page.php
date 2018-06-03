4e51e0a5469648f871bf48b7e28fc99264b89188
Daniel Brown <fireflip@gmail.com>
2018-06-03 12:07:44 -0500

    Inner page structure

--- page.php
+++ page.php
@@ -1,15 +1,15 @@
 <!--page-->
 <?php get_header(); ?>
 
-			<div id="content" class="<?php echo sanitize_title_with_dashes(get_the_title()); ?>">
+			<div id="content">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<header class="article-header">
+					<header class="article article__header wrap">
 
 
-						<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+						<h1 class="page-title <?php echo sanitize_title_with_dashes(get_the_title()); ?>" itemprop="headline"><?php the_title(); ?></h1>
 
 					</header>
-				<div id="inner-content" class="wrap cf row">
+				<div id="inner__content" class="wrap cf row">
 
 						<main id="main" class="col-xs-12 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
@@ -19,25 +19,65 @@
 
 
 
-								<section class="entry-content cf" itemprop="articleBody">
+								<section class="entry__content cf" itemprop="articleBody">
 
 									<?php the_content(); ?>
 
 								</section> <?php // end article section ?>
 
-								<footer class="article-footer cf">
 
-								</footer>
 
 								<?php comments_template(); ?>
 
 							</article>
 
-							<?php endwhile; endif; ?>
 
 						</main>
 
 				</div>
+				<section class="content__full-width content__full-width--lt-blue content__full-width--centered <?php echo sanitize_title_with_dashes(get_the_title()); ?>">
+					<div class="wrap">
+						<h1>Section Title</h1>
+						<ul class="col-xs-12">
+							<li class="col-xs-12 col-sm-6">
+								<a href="#">
+									<h4>ANI Current Medical Information Form</h4>
+									<p>
+										Form for release of health history from another provider to ANI
+									</p>
+								</a>
+							</li>
+							<li class="col-xs-12 col-sm-6">
+								<a href="#">
+									<h4>ANI Current Medical Information Form</h4>
+									<p>
+										Form for release of health history from another provider to ANI
+									</p>
+								</a>
+							</li>
+							<li class="col-xs-12 col-sm-6">
+								<a href="#">
+									<h4>ANI Current Medical Information Form</h4>
+									<p>
+										Form for release of health history from another provider to ANI
+									</p>
+								</a>
+							</li>
+							<li class="col-xs-12 col-sm-6">
+								<a href="#">
+									<h4>ANI Current Medical Information Form</h4>
+									<p>
+										Form for release of health history from another provider to ANI
+									</p>
+								</a>
+							</li>
+
+
+						</ul>
+					</div>
+				</section>
+
+				<?php endwhile; endif; ?>
 
 			</div>
 
