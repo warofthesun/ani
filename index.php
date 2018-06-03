ac3b8b538d81636e249942660368cb86e3a14f52
Daniel Brown <fireflip@gmail.com>
2018-06-03 15:16:59 -0500

    Make separate loops for most recent post and all others

--- index.php
+++ index.php
@@ -2,25 +2,39 @@
 <?php get_header(); ?>
 
 			<div id="content">
+				<header class="article article__header wrap">
 
+						<?php if ( is_front_page() && is_home() ) {
+							  // Default homepage
+							} elseif ( is_front_page() ) {
+							  // static homepage
+							} elseif ( is_home() ) { ?>
+							<?php	$page_title = get_the_title( get_option('page_for_posts', true) ); ?>
+							<h1 class="page-title blog-page" itemprop="headline"><?php echo $page_title; ?></h1>
+						<?php	} else { ?>
+							  <h1 class="page-title <?php echo sanitize_title_with_dashes(get_the_title()); ?>" itemprop="headline"><?php echo $page_title; ?></h1>
+								<?php
+							}
+						?>
+
+				</header>
 				<div id="inner-content" class="wrap cf row">
 
 						<main id="main" class="col-xs-12 col-sm-8 col-lg-9 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
+							<?php $the_query = new WP_Query( 'showposts=1&offset=1' ); ?>
+						<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf single-post' ); ?> role="article">
 
 								<header class="article-header">
 
 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 									<p class="byline entry-meta vcard">
-	                      <?php printf( __( 'Posted', 'startertheme' ).' %1$s %2$s',
-	       								/* the time the post was published */
-	       								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-	       								/* the author of the post */
-	       								'<span class="by">'.__( 'by', 'startertheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-	    							); ?>
+												<?php printf( __( 'Posted', 'startertheme' ).' %1$s %2$s',
+												/* the time the post was published */
+												'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+												/* the author of the post */
+												'<span class="by">'.__( 'by', 'startertheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+										); ?>
 									</p>
 
 								</header>
@@ -35,34 +49,54 @@
 									</p>
 
 
-                 	<?php printf( '<p class="footer-category">' . __('filed under', 'startertheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
+									<?php printf( '<p class="footer-category">' . __('filed under', 'startertheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
 
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'startertheme' ) . '</span> ', ', ', '</p>' ); ?>
+									<?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'startertheme' ) . '</span> ', ', ', '</p>' ); ?>
 
 
 								</footer>
 
 							</article>
+						<?php endwhile;?>
+						<?php $the_query = new WP_Query( 'offset=1' ); ?>
+					<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf single-post' ); ?> role="article">
 
-							<?php endwhile; ?>
+							<header class="article-header">
 
-									<?php starter_page_navi(); ?>
+								<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+								<p class="byline entry-meta vcard">
+											<?php printf( __( 'Posted', 'startertheme' ).' %1$s %2$s',
+											/* the time the post was published */
+											'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+											/* the author of the post */
+											'<span class="by">'.__( 'by', 'startertheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+									); ?>
+								</p>
 
-							<?php else : ?>
+							</header>
 
-									<article id="post-not-found" class="hentry cf">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'startertheme' ); ?></h1>
-										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'startertheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the index.php template.', 'startertheme' ); ?></p>
-										</footer>
-									</article>
+							<section class="entry-content cf">
+								<?php the_excerpt(); ?>
+							</section>
 
-							<?php endif; ?>
+							<footer class="article-footer cf">
+								<p class="footer-comment-count">
+									<?php comments_number( __( '<span>No</span> Comments', 'startertheme' ), __( '<span>One</span> Comment', 'startertheme' ), __( '<span>%</span> Comments', 'startertheme' ) );?>
+								</p>
+
+
+								<?php printf( '<p class="footer-category">' . __('filed under', 'startertheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
+
+								<?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'startertheme' ) . '</span> ', ', ', '</p>' ); ?>
+
+
+							</footer>
+
+						</article>
+					<?php endwhile;?>
+
+
 
 
 						</main>
