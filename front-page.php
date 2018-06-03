abc37f770fe8e0d1e9de53646672721e7b8a69eb
Daniel Brown <fireflip@gmail.com>
2018-06-02 21:06:55 -0500

    Front page structure in place

--- front-page.php
+++ front-page.php
@@ -1,17 +1,19 @@
 <!--front page-->
 <?php get_header(); ?>
-			<div class="hero">
+			<section class="hero">
 				<div class="hero__image hero__image--full">
 					<div class="hero__content hero__content--left wrap">
-						Your Brain &amp; Nerve Neurology Experts
+						<p>
+							Your Brain &amp;<br />Nerve Neurology<br />Experts
+						</p>
 					</div>
 				</div>
 				<div class="hero__content">
 					<div class="content-boxes row wrap">
 						<div class="col-xs-12 col-sm-3">
-							<div class="content-boxes__header">
+							<a href="#" class="content-boxes__header">
 								<p>Making an<br />Appointment</p>
-							</div>
+							</a>
 							<div class="content-boxes__content">this is the content</div>
 						</div>
 						<div class="col-xs-12 col-sm-3">
@@ -34,8 +36,8 @@
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="content--secondary">
+			</section>
+			<section class="content--secondary">
 				<div class="content-boxes wrap row">
 					<div class="col-xs-12 col-sm-4">
 						<div class="content-boxes__header">
@@ -59,14 +61,78 @@
 						<div class="content-boxes__content">this is the content</div>
 					</div>
 				</div>
-			</div>
-
-			<section class="content__full-width content__full-width--centered row">
+			</section>
+			<section class="content__full-width content__full-width--lt-blue content__full-width--centered">
 				<div class="wrap">
 					<h1>About Us</h2>
 					<p>
 						Bitly blyve oovoo zynga whrrl, hojoki plugg grockit wikia, zoosk skype greplin. Sococo kiko trulia imvu, stypi. Zanga heroku boxbe jiglu kno, babblely imeem ning. Elgg loopt oooj jajah vuvox twones lala hojoki, boxbe chartly heekya nuvvo kaboodle jiglu. Revver insala squidoo sifteo rovio, sclipo meevee ifttt. Tivo lala blippy oovoo, xobni. fleck doostang. zillow quora spock. Grockit wikia omgpop empressr chartly, plaxo lijit. Imvu hipmunk plaxo blippy whrrl, blyve bubbli zillow. Meevee flickr zooomr nuvvo kno, zapier waze. Zapier jaiku reddit shopify zillow lala nuvvo, sclipo joyent xobni sclipo.
 					</p>
+				</div>
+			</section>
+			<section class="content staff wrap row">
+				<h1 class="col-xs-12">Our Staff</h1>
+				<div class="col-xs-12 col-sm-4">
+					<div>
+						<div class="staff__name">Mary<br />White</div>
+						<div class="staff__role">Their Role</div>
+						<div class="staff__qualifications">Their Qualifications</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-4">col</div>
+				<div class="col-xs-12 col-sm-4">col</div>
+				<a href="#" class="col-xs-12" style="text-align:right;">See All</a>
+			</section>
+			<section class="content content__full-width content__full-width--centered">
+				<div class="wrap recent-news">
+					<h1>Recent News</h2>
+					<div class="flex-direction flex-direction--row">
+					<div class="col-xs-12 col-sm-2 col-md-3">
+						<div class="recent-news__category">Category</div>
+						<img src="https://picsum.photos/300" />
+						<div class="recent-news__title">Post Title</div>
+						<div class="recent-news__date">Oct. 15</div>
+						<p>Bitly blyve oovoo zynga whrrl, hojoki plugg grockit wikia…</p>
+					</div>
+					<div class="col-xs-12 col-sm-2 col-md-3">
+						<div class="recent-news__category">Category</div>
+						<img src="https://picsum.photos/400" />
+						<div class="recent-news__title">Post Title</div>
+						<div class="recent-news__date">Oct. 15</div>
+						<p>Bitly blyve oovoo zynga whrrl, hojoki plugg grockit wikia…</p>
+					</div>
+					<div class="col-xs-12 col-sm-2 col-md-3">
+						<div class="recent-news__category">Category</div>
+						<img src="https://picsum.photos/350" />
+						<div class="recent-news__title">Post Title</div>
+						<div class="recent-news__date">Oct. 15</div>
+						<p>Bitly blyve oovoo zynga whrrl, hojoki plugg grockit wikia…</p>
+					</div>
+					<div class="col-xs-12 col-sm-2 col-md-3">
+						<div class="recent-news__category">Category</div>
+						<img src="https://picsum.photos/450" />
+						<div class="recent-news__title">Post Title</div>
+						<div class="recent-news__date">Oct. 15</div>
+						<p>Bitly blyve oovoo zynga whrrl, hojoki plugg grockit wikia…</p>
+					</div>
+					</div>
+				</div>
+			</section>
+			<section class="content content__full-width content__full-width--dk-blue collapse--bottom">
+				<div class="contact wrap">
+					<h1 class="col-xs-12 ">Contact Us</h1>
+					<div class="flex-direction flex-direction--row">
+
+							<p class="col-xs-12 col-md-5 collapse--top">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis ipsum suscipit, dignissim libero id, varius ligula. Integer sem nunc, tristique eu cursus at, posuere at enim. Pellentesque tristique molestie condimentum. Nam venenatis tincidunt enim eget semper. Nulla non ante vitae felis molestie sodales. Mauris quis risus sed neque scelerisque aliquam et ac metus. Duis egestas ligula eu odio interdum tempus. Maecenas varius mauris libero, sed lacinia magna maximus nec.
+							</p>
+
+						<div class="col-xs-12 col-md-7" style="margin-top:-1em;">
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+								<?php the_content(); ?>
+							<?php endwhile; endif; ?>
+						</div>
+					</div>
 				</div>
 			</section>
 
