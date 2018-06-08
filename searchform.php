aa49cc771fe687a4b0fd578a7ffbe7862487572a
Daniel Brown <fireflip@gmail.com>
2018-06-07 20:20:59 -0500

    Style search form

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
-        <label for="s" class="screen-reader-text"><?php _e('Search for:','startertheme'); ?></label>
-        <input type="search" id="s" name="s" value="" />
+        <label for="s" class="screen-reader-text"></label>
+        <input type="search" id="s" name="s" value="" placeholder="Search"/>
 
-        <button type="submit" id="searchsubmit" ><?php _e('Search','startertheme'); ?></button>
+        <button type="submit" id="searchsubmit"><i class="fa fa-search" aria-hidden="true"></i></button>
     </div>
 </form>
