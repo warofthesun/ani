91ef48af800d19b8cb10b75fcb8f9fce7e161ae2
Daniel Brown <fireflip@gmail.com>
2018-06-16 18:28:01 -0500

    Add optional full width lower section to page template

--- page.php
+++ page.php
@@ -35,45 +35,30 @@
 						</main>
 
 				</div>
+				<?php if ( get_field( 'include_bottom_section_tf' ) ): ?>
 				<section class="content__full-width content__full-width--lt-blue content__full-width--centered <?php echo sanitize_title_with_dashes(get_the_title()); ?>">
 					<div class="wrap">
-						<h1>Section Title</h1>
-						<ul class="col-xs-12">
+						<h1><?php the_field('lower_section_title'); ?></h1>
+						<?php if( get_field('which_page_is_this_for') == 'making_an_appointment' ): ?>
+						<ul class="row">
+							<?php if( have_rows('document_library') ): while ( have_rows('document_library') ) : the_row(); ?>
 							<li class="col-xs-12 col-sm-6">
-								<a href="#">
-									<h4>ANI Current Medical Information Form</h4>
+								<a href="<?php the_sub_field('link_to_form'); ?>">
+									<h4><?php the_sub_field('title_of_form'); ?></h4>
 									<p>
-										Form for release of health history from another provider to ANI
+										<?php the_sub_field('brief_description'); ?>
 									</p>
 								</a>
 							</li>
-							<li class="col-xs-12 col-sm-6">
-								<a href="#">
-									<h4>ANI Current Medical Information Form</h4>
-									<p>
-										Form for release of health history from another provider to ANI
-									</p>
-								</a>
-							</li>
-							<li class="col-xs-12 col-sm-6">
-								<a href="#">
-									<h4>ANI Current Medical Information Form</h4>
-									<p>
-										Form for release of health history from another provider to ANI
-									</p>
-								</a>
-							</li>
-							<li class="col-xs-12 col-sm-6">
-								<a href="#">
-									<h4>ANI Current Medical Information Form</h4>
-									<p>
-										Form for release of health history from another provider to ANI
-									</p>
-								</a>
-							</li>
+						<?php endwhile; endif; ?>
 						</ul>
+					<?php elseif( get_field('which_page_is_this_for') == 'directions' ): ?>
+						<?php the_field('maps'); ?>
+					<?php else: endif; ?>
 					</div>
 				</section>
+			<?php else: ?>
+			<?php endif; ?>
 
 				<?php endwhile; endif; ?>
 
